f0622d27c8dcb01e443352af49388111b2b9099b
Decima <jdecima@cablevision.com.ar>
2018-04-03 15:34:13 -0300

    Se agregar User a Modelado Se mejora modelo de pedidos

--- src/apps/order/models.py
+++ src/apps/order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Se crea modelo para los estados
 class Estado(models.Model):
@@ -6,9 +7,27 @@ class Estado(models.Model):
     nombre_estado = models.CharField(max_length=30)
 
 # Se crea modelo de pedidos
-class Pedido (models.Model):
-    # idCliente
-    # idPedido
-    # idItem
-    estado = models.ForeignKey(Estado, null=False, blank=False, on_delete=models.CASCADE) #Clae foranea de Estados
+class Pedido(models.Model):
+    id_cliente = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    id_pedido = models.IntegerField(primary_key=True)
+    estado = models.ForeignKey(Estado, null=False, blank=False, on_delete=models.CASCADE) #Clave foranea de Estados
 
+#Se crea modelo de materiales para impresion
+class Material(models.Model):
+    id_mat = models.IntegerField(primary_key=True)
+    id_imp = ForeignKey(Impresora, null=False, blank=False, on_delete=models.CASCADE)
+    nombre_mat = models.CharField(max_length=50)
+
+#Se modela las impresoras
+class Impresora(models.Model):
+    id_imp = models.IntegerField(primary_key=True)
+    nombre_imp = models.CharField(max_length=50)
+
+# Se crea mdoelo para Items
+class Item(models.Model):
+    id_item = models.IntegerField(primary_key=True)
+    id_pedido = models.ForeignKey(Pedido, blank=False, on_delete=models.CASCADE)
+    sys_impr = ForeignKey(Impresora, null=False, blank=False, on_delete=models.CASCADE)
+    tipo_material = ForeignKey(Material, null=True, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    observacion = models.models.TextField()
--- src/apps/profiles/models.py
+++ src/apps/profiles/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Perfil, Modelado de perfil en base de datos.
 class Profile(models.Model):
-    cuenta = models.ForeignKey(auth.user)
+    cuenta = models.OneToOneField(User, on_delete=models.CASCADE)   # Se enlaza uno a uno con los usuarios
     direccion = models.TextField()
     telefono = models.CharField(max_length = 15)
     correo = models.EmailField()
+    id_cliente = models.IntegerField()
+    fecha_nacimiento = models.DateField(null=True, blank=True)
