2dfc470c2b81777beb386cb319d6b51b6c10245a
Pato <joaquin.decima@gmail.com>
2018-04-12 11:55:25 -0300

    Se Crea Registro

--- src/apps/profiles/urls.py
+++ src/apps/profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from apps.profiles.views import index
+from apps.profiles.views import index, RegistroUsuario
 
 urlpatterns = [
     path('', index, name = 'inicio'),
+    path('registro', RegistroUsuario.as_view(), name = 'registro'),
 ]
--- src/apps/profiles/views.py
+++ src/apps/profiles/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from apps.profiles.forms import RegistroForm
 
 # Create your views here.
 def index(request):
     return render(request, 'board/pages/inicio.html')
+
+class RegistroUsuario(CreateView):
+    model = User
+    template_name = "board/forms/registro.html"
+    form_class = RegistroForm
+    success_url = reverse_lazy('perfil:inicio')
