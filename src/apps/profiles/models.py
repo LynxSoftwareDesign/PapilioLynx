39289d2df29dcab2387af4511cb075872587deae
Pato <joaquin.decima@gmail.com>
2018-04-22 16:03:08 -0300

    Se inicia model tipo_cliente Se modifica Menu Board

--- src/apps/profiles/models.py
+++ src/apps/profiles/models.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Perfil, Modelado de perfil en base de datos.
+# Se modela Tipos de Cuentas
+class TipoCuenta(models.Model):
+    nombreTipo = models.CharField(max_length = 25)
+    #adm_menu = models.boolean()
+
+# Modelado de perfil.
 class Profile(models.Model):
     cuenta = models.OneToOneField(User, on_delete=models.CASCADE)   # Se enlaza uno a uno con los usuarios
-    direccion = models.TextField(null=True, blank=True)
+    tipo_cuenta = models.OneToOneField(TipoCuenta, on_delete=models.CASCADE, null=True, blank=True)
+    direccion = models.CharField(max_length = 50, null=True, blank=True)
     telefono = models.CharField(max_length = 15, null=True, blank=True)
     fecha_nacimiento = models.DateField(null=True, blank=True)
